21ff69320102de6908542e797b17bbfdf41752de
arryadali <arryadali@gmail.com>
2023-11-05 21:36:18 +0700

    first commit

---
+++ database/data.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        id : 1,
+        question : "Saya adalah ........ Dali Lesmana",
+        options : [
+            'Arrya',
+            'Arya',
+            'Aryya',
+        ]
+    },
+    {
+        id : 2,
+        question : "Saya adalah ........ Bagus",
+        options : [
+            'Arrya',
+            'Arya',
+            'Aryya',
+        ]
+    },
+    {
+        id : 3,
+        question : "Saya adalah ........ Satya Okta",
+        options : [
+            'Arrya',
+            'Arya',
+            'Aryya',
+        ]
+    }
+]
+
+export const answers = [0, 2, 1];
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
 import {config} from 'dotenv';
+import router from "./router/route.js";
+
+// Import connection file
+import connect from "./database/conn.js";
 
 const app = express()
 
@@ -15,6 +19,9 @@ config();
 const port = process.env.PORT || 8080;
 
 // routes
+app.use('/api', router)
+
+
 app.get('/', (req, res) => {
     try {
         res.json("Get Request")
@@ -23,6 +30,15 @@ app.get('/', (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server connected to http://localhost:${port}`)
+// start server only when we have valid connection
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log("Cannot connect to the server")
+    }
+}).catch(error => {
+    console.log("Invalid Database Connection")
 })
