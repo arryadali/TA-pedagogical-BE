ca1cfd76c1e7b68c421192c20ddd8257b7aa1c45
arryadali <arryadali@gmail.com>
2023-11-15 15:32:01 +0700

    new

--- database/materi.js
+++ database/materi.js
@@ -2,21 +2,34 @@ export default [
     {
         id : 1,
         judul : "Step 1",
-        konten : "awidbuawhdiuawhudwad.",
+        konten : `Jika dia membutuhkan 3/4 jumlah tepung kue, maka dia perlu 3/4 dari 2 1/3 cangkir. Menjadi 
+        
+        2 1/3 x 3/4 = 
+        `,
     },
     {
         id : 2,
         judul: "Step 2",
-        konten : "Isi konten untuk Langkah 2.",
+        konten : `Untuk mengalikan bilangan campuran dan pecahan, pertama-tama jadikan bilangan campuran tersebut menjadi pecahan biasa.
+
+        2 1/3 => (2 x 3) + 1 / 3 => 7/3
+        `,
     },
     {
         id : 3,
         judul: "Step 3",
-        konten : "Isi konten untuk Langkah 3.",
+        konten : `Untuk mengalikan pecahan, kalikan pembilang dan penyebutnya, lalu sederhanakan. 
+        
+        7/3 x 3/4 = 7x3 / 3x4 = 7x3 / 3x4 = 7/4
+
+        Sekarang, ubahlah menjadi bilangan campuran!
+        `,
     },
     {
         id : 4,
         judul: "Step 4",
-        konten : "Isi konten untuk Langkah 4.",
+        konten : `Jawaban yang benar adalah 
+        1 3/4
+        `,
     },
 ]
